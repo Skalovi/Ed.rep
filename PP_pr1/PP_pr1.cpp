d14ed93000269ce5c60eae392d48474cdf03d602
Skalovi <crimson.skalovi@gmail.com>
2021-09-12 21:46:11 +0500

    Добавлен вывод произведения и частного чисел.

--- PP_pr1/PP_pr1.cpp
+++ PP_pr1/PP_pr1.cpp
@@ -9,7 +9,7 @@ int main()
     cout << "Number A: ";
         cin >> a;
     cout << "Number B: ";
-        cin >> b;
-        cout << "A+B=" << a + b << "\nA-B=" << a - b << "\nAverage=" << (abs(a)+abs(b))/2;
+        cin >> b; 
+        cout << "A+B = " << a + b << "\nA-B = " << a - b << "\nAverage = " << (abs(a)+abs(b))/2 << "\nA*B=" << a * b << "\nA/B=" << 1.0 * a / b;
     return 0;
 }
