362f9fce89dff5a1b837619abfdc48a6f0dbaaf3
skalolazer <skalolazer@DESKTOP-UC56JUL>
2021-09-12 20:41:40 +0500

    Добавьте файлы проекта.

---
+++ PP_pr1/PP_pr1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Number A: ";
+        cin >> a;
+    cout << "Number B: ";
+        cin >> b;
+        cout << "A+B=" << a + b << "\nA-B=" << a - b << "\nAverage=" << (abs(a)+abs(b))/2;
+    return 0;
+}
